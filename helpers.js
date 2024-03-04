070b6a556fc282d2e4be597efe4bd02de6d2fef4
Ethspresso <ethspresso@gmail.com>
2024-03-04 17:59:36 +0100

    Use local image hosting instead of IPFS

--- helpers.js
+++ helpers.js
@@ -1,13 +1,15 @@
+const APP_URL = "https://nftoshi-explorer-ethspresso.koyeb.app";
+
 const MARKETPLACE_URL = "https://opensea.io/assets";
 const IPFS_PROVIDER_URL = "https://cloudflare-ipfs.com/ipfs";
 
 const COLLECTION_NETWORK = "base";
 const COLLECTION_CONTRACT = "0xbdb1a8772409a0c5eeb347060cbf4b41dd7b2c62";
 const IPFS_IMAGE_HASH = "bafybeicgkb56vydl3kkcrgox7dpyatrgafxogurp42opjmiocfnwmbftlm";
-const IPFS_IMAGE_FILETYPE = ".png";
+const IMAGE_FILETYPE = ".png";
 
 function getImgUrl(token_id) {
-  return IPFS_PROVIDER_URL + "/" + IPFS_IMAGE_HASH + "/" + token_id + IPFS_IMAGE_FILETYPE;
+  return APP_URL + "/img/" + token_id + IMAGE_FILETYPE;
 };
 
 function getMarketplaceUrl(token_id) {
